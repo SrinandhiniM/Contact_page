89235f37eae9ed974fb7e74f760def48fa694edd
Srinandhini M <srinandhini@sedintechnologies.com>
2022-09-27 11:40:42 +0530

    Edited the page

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,21 +1,41 @@
 class ContactsController < ApplicationController
-  def index
-    @contacts = Contact.all
+  before_action :find_contact, only: [:edit, :update, :destroy]
+    def index
+      @contacts = Contact.all
+    end
+    def new
+     @contact = Contact.new
+    end
+    def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      flash[:success] = "Successfully created new contact"
+      redirect_to contacts_path
+    else
+      render 'new'
+    end
+    end
+    def edit
+    end
+    def update
+      if @contact.update(contact_params)
+        flash[:success] = "Successfully Update contact"
+        redirect_to contacts_path
+      else
+        render 'edit'
+      end
+    end
+    def destroy
+      if @contact.destroy
+        flash[:success] = "Successfully Deleted contact"
+        redirect_to contacts_path
+      end
+    end
+    def find_contact
+      @contact = Contact.find(params[:id])
+    end
+    private
+    def contact_params
+      params.require(:contact).permit(:name, :email, :company, :address ,:phone ,:dob)
+    end
   end
-  def new
-   @contact = Contact.new
-  end
-  def create
-  @contact = Contact.new(contact_params)
-  if @contact.save
-    flash[:success] = "Successfully created new contact"
-    redirect_to contacts_path
-  else
-    render 'new'
-  end
-  end
-  private
-  def contact_params
-    params.require(:contact).permit(:name, :email, :company, :adress ,:phone ,:dob)
-  end
-end
